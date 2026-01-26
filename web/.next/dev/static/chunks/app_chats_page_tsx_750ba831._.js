522dd53ef8bba679dee93cfc818f6ca49b6c02e6
Arefinw <arefinhossainw@gmail.com>
2026-01-26 12:46:49 +0600

    removed senstive files from tracking

--- web/.next/dev/static/chunks/app_chats_page_tsx_750ba831._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_e754a902._.js",
-  "static/chunks/_2bb41e63._.js"
-],
-    source: "dynamic"
-});
--- web/.next/dev/static/chunks/app_login_page_tsx_750ba831._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_b4f37fa8._.js",
-  "static/chunks/_755883ea._.js"
-],
-    source: "dynamic"
-});
